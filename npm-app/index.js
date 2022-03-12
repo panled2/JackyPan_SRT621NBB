eb27be55b1d46176ab792e1790c9490911a1d2ef
panled2 <jacky.pan32@gmail.com>
2022-03-12 12:37:27 -0500

    Finished Lab

---
+++ npm-app/index.js
@@ -0,0 +1,3 @@
+exports.time = function (){
+    return Date();
+};
--- web-server/index.js
+++ web-server/index.js
@@ -1,11 +1,13 @@
 const port = 8989,
 http = require("http"),
-httpStatus = require("http-status-codes"),
+x= require('../npm-app/index'),
+httpStatus = require("http-status-codes"),   
 app = http.createServer((requeset, response) => {
     console.log("Received an incoming request!");
     response.writeHead(httpStatus.OK, {
         "Content-Type": "text/html"
     });
+    response.write("The current date and time is: " + x.time());
     response.end();
 });
 
